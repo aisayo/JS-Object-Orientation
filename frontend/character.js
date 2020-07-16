d87fa29fe3f14ad44df754f4135f84ee590f6bde
Aysan Isayo <aisayo@icloud.com>
2020-07-16 16:05:22 -0700

    final code

---
+++ frontend/character.js
@@ -0,0 +1,14 @@
+class Character {
+    constructor(name, show){
+        this.name = name
+        this.show = show
+    }
+
+    //instance method || standard method
+    renderCharacter(){
+        //do something in this method 
+    }
+
+
+
+}
--- frontend/index.js
+++ frontend/index.js
@@ -1,46 +1,63 @@
 //creating a class
 
-class Character {
-    constructor(name, show) {
-      this.name = name
-      this.show = show
-    }
-   
-    //instance method
-    sayHello() {
-      return `Hi, I am ${this.name} from ${this.show}`
-    }
+// document.addEventListener("DOMContentLoaded", () => {
+//     example();
+// })
 
-    renderCharacter() {
-        const body = document.body
-        body.innerHTML = 
-        ` ${this.name} from ${this.show} `
-    }
-  }
+// class Character {
+//     constructor(name, show){
+//         this.name = name
+//         this.show = show
+//     }
+//    //instance method
+    // sayHello() {
+    //     console.log(this)
+    //   return `Hi, I am ${this.name} from ${this.show}`
+    // }
+
+//     renderCharacter() {
+//         const body = document.body
+//         body.innerHTML = 
+//         ` 
+//             <ul>
+//             <li>${this.name} from ${this.show}</li>
+//             </ul>
+//         `
+//     }
+//   }
+
+  // let character = new Character("Tommy", "Rugrats");
+//   console.log(character.sayHello())
+
+
+
    
 //instantiating a new instance of character class
 
-  const createCharacter = () => {
-    let character = new Character("Tommy", "Rugrats");
-    console.log(character); 
-    console.log(character.name); 
-    return character.renderCharacter(); 
-  }
+//   const createCharacter = () => {
+//     console.log(character); 
+//     console.log(character.name); 
+//     return character.renderCharacter(); 
+//   }
 
 
 //using bind
+
 //   const tommy = {
 //       name: "Tommy",
 //       show: "Rugrats"
 //   }
 
-//   function whatShow() {
-//       return `I am from ${this.show}`
-//   }
+// //   function whatShow() {
+// //       return `I am from ${this.show}`
+// //   }
 
-//   console.log(whatShow())
-//   let charShow = whatShow.bind(tommy)
-//   console.log(charShow())
+// const whatShow = () => {
+//     console.log("inside arrow function", this)
+// }
+// //   console.log(whatShow())
+// let charShow = whatShow.bind(tommy)
+// console.log("binding", charShow())
 
   
 // What gives? and how to apply with a Rails backend??
@@ -49,7 +66,7 @@ class Character {
 // 2. resolve the data with first .then()
 // 3. in second .then(), will create new objects with data received
 
-//using call
+//using call()
 
 // function getBrand(prefix) {
 //     console.log(prefix + this.brand);
@@ -62,19 +79,28 @@ class Character {
 //     brand: 'Audi'
 // };
 
-// getBrand.call(honda, "It's a ");
-// getBrand.call(audi, "It's an ");`
+// getBrand.apply(honda, [1, "test", true]);
+// getBrand.apply(audi, [2, "driving", false]);
 
 // dummy code
 
-//index fetch request
+//index fetch request - return all character objects from backend
+
+
 // fetch("https://bogus.com/characters")
 // .then(resp => resp.json())
 // .then(characterArray => 
 //     //since this is an index, will get an array of objects
 //     //need to iterate over array and create JS objects with it
 //     characterArray.array.forEach(char => {
+
+
 //         let character = new Character(char.name, char.show)
 //         character.renderCharacter();
+
+//         //creating new objects with the incoming data
+//         //aappending to dom 
+
+
 //     })
 // )
